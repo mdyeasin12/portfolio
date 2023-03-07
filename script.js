6324443339ac3361e03a6a51fe160c75ffe7938c
MD YEASIN <118657902+mdyeasin12@users.noreply.github.com>
2023-03-07 15:28:21 +0600

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["", "Developer", "Content Writter", "Copywritter ", "Front-End Web Developer", "Freelancer"],
+        strings: ["", "Developer", "Student", "Content Writter", "Copywritter", "Front-End Web Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Content Writter", " Copywritter ", "Front-End Web Developer", "Freelancer"],
+        strings: ["Developer", "Student", "Content Writter", " Copywritter", "Front-End Web Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
